a374531d3103e85231ee5ff18fb51492cf28e4fa
bshetty537 <shettyb@clarkson.edu>
2025-04-21 15:39:02 -0400

    Update customer_details.sql
    Commit: Updated customer_details.sql file with inventory details

--- SQL/customer_details.sql
+++ SQL/customer_details.sql
@@ -1,17 +1,22 @@
+-- Table: public.Customer_details
+
 DROP TABLE IF EXISTS public."Customer_details";
 
 CREATE TABLE IF NOT EXISTS public."Customer_details"
 (
-    "CustomerID" character(1) COLLATE pg_catalog."default",
-    "Reservations" character(1) COLLATE pg_catalog."default",
-    "Name" character(1) COLLATE pg_catalog."default",
-    "Email_Id" character(1) COLLATE pg_catalog."default",
-    "Phone_num" numeric,
-    "Preferences" character(1) COLLATE pg_catalog."default",
-    "Payment_history" numeric,
+    "Reservations" character(20) COLLATE pg_catalog."default",
+    "Name" character(20) COLLATE pg_catalog."default",
+    "Email_Id" character(20) COLLATE pg_catalog."default",
+    "Phone_num" numeric(10,0),
+    "Preferences" character(20) COLLATE pg_catalog."default",
+    "Payment_history" numeric(10,0),
     "Check_In_Date" date,
     "Check_Out_Date" date,
-    "Loyalty programs" numeric
+    "Loyalty_programs" numeric(10,0),
+    "CustomerID" integer NOT NULL DEFAULT nextval('"Customer_details_CustomerID_seq"'::regclass),
+    "Inventory" character(50) COLLATE pg_catalog."default",
+    "Inventory_price" numeric(10,2),
+    CONSTRAINT "Customer_details_pkey" PRIMARY KEY ("CustomerID")
 )
 
 TABLESPACE pg_default;
