80abca8ae5ffbcd79d63f7e14873f6659b52875d
Ashikahamedpranto <119426599+Ashikahamedpranto@users.noreply.github.com>
2025-04-26 22:00:35 -0400

    Update public_credentials.sql

--- SQL/public_credentials.sql
+++ SQL/public_credentials.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS public."Credentials";
 CREATE TABLE IF NOT EXISTS public."Credentials"
 (
     "Username" character(10) COLLATE pg_catalog."default" NOT NULL,
+    
     "Password" character(15) COLLATE pg_catalog."default",
     CONSTRAINT "Credentials_pkey" PRIMARY KEY ("Username")
 )
